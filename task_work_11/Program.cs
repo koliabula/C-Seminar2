8777424832969fff12e98f6e72f7b2b4c473ff53
Bulla Kolia <koliakolan@gmail.com>
2022-09-05 12:33:24 +0300

    Семинар Задача 11 добавил Clear

--- task_work_11/Program.cs
+++ task_work_11/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и 
 // удаляет вторую цифру этого числа
-
+Console.Clear();
 Console.Write("Трёхзначное число: ");
 int number = new Random().Next(100, 1000);
 Console.WriteLine( number);
