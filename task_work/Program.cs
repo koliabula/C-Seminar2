ad85f4d3a24e211bc1c7d3b099def9270e3cabb9
Bulla Kolia <koliakolan@gmail.com>
2022-09-05 13:08:49 +0300

    Семинар Задача

---
+++ task_work/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход 2 часла и проверяет,
+// является ли одно число квадратом другого
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2){
+    Console.Write($"Число {number2} - квадрат числа {number1} ");
+}
+
+else if(number2 * number2 == number1){
+    Console.Write($"Число {number1} - квадрат числа {number2} ");
+}
+
+else Console.Write("НОУ");
