b5ffa523a105367a8516038486fcdbc4ee5443e6
Bulla Kolia <koliakolan@gmail.com>
2022-09-05 12:44:11 +0300

    Семинар Задача 14

---
+++ task_work_14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверает, 
+// кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 & number % 23 == 0){
+    Console.Write($"Число {number} ДЕЛИТСЯ одновременно на 7 и 23 ");
+}
+else{
+    Console.Write($"Число {number} НЕ ДЕЛИТСЯ одновременно на 7 и 23 ");
+}
