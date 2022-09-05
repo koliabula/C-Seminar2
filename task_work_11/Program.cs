72e5f9138a7b85c1f73bf325e0b85801df28b66a
Bulla Kolia <koliakolan@gmail.com>
2022-09-05 12:29:56 +0300

    Семинар Задача 11

---
+++ task_work_11/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа
+
+Console.Write("Трёхзначное число: ");
+int number = new Random().Next(100, 1000);
+Console.WriteLine( number);
+Console.Write("Удаляем вторую цифру: ");
+int number2 = number/100*10+number%10;
+Console.Write(number2);
